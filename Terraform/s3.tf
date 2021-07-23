7f6e488b73197da42caae3b82f36959d94837caf
Praveenrtp <46226861+Praveenrtp@users.noreply.github.com>
2021-07-22 21:29:05 -0400

    Fixed typo

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -6,20 +6,22 @@ resource "aws_s3_bucket" "mywebsite_react" {
   bucket        = "${local.prefix}-app"
   acl           = "public-read"
   force_destroy = true
-  policy        = <<EOF
-  {
-      "Version" = "2012-10-17",
-      "Statement" = [
-      {
-        "Sid"       = "IPAllow"
-        "Effect"    = "Deny"
-        "Principal" = "*"
-        "Action"    = "s3:GetObject"
-        "Resource"  = "arn:aws:s3:::${local.prefix}-app/*"
-      }
-    ]
-  }
-  EOF
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${local.prefix}-app/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
 
   website {
     index_document = "index.html"
